c2ff7d6e58d889e6ebc2d109795e5ef26d32914a
Jan Hazincak <jan.hazincak@gmail.com>
2021-01-04 11:28:55 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        int portNumber;
+        int menuChoice;
+
+        menuChoice = menu();
+        if(menuChoice != 0){
+            portNumber = portNumber();
+            System.out.println(portNumber
+            );
+        }
+    }
+
+    public static int menu(){
+        Scanner input = new Scanner(System.in);
+        int selection;
+
+
+        System.out.println("Welcome to ChatServer configuration.");
+        System.out.println("Select your option");
+        System.out.println("-------------------------\n");
+        System.out.println("1 - Create a server");
+        System.out.println("2 - Quit");
+
+        selection = input.nextInt();
+        return selection;
+    }
+
+    public static int portNumber(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Select a port number for your server (0 - 65,535 inclusive)");
+        String portString = input.nextLine();
+        int portNumber = 0 ;
+        boolean portNumberValid = false;
+
+        while(true){
+            try{
+                portNumber = Integer.parseInt(portString);
+                if(portNumber >=0 && portNumber <= 65535){
+                    System.out.println("Port number of the server set to  " + portNumber);
+                    break;
+                }else if(portNumber < 0){
+                    System.out.println("Port cannot be a negative number");
+                }else if (portNumber > 65535) {
+                    System.out.println("Port number cannot be higher than 65535");
+                }
+                System.out.println("Select a port number for your server (0 - 65,535 inclusive)");
+                portString = input.nextLine();
+
+            } catch (NumberFormatException e){
+                System.out.println("Invalid portnumber");
+                System.out.println("Try again");
+                portString = input.nextLine();
+            }
+        }
+        return portNumber;
+    }
+
+}
